8b103f8a4bb32361ebb744124f482742677702e4
ToshaZ <tsterk.hzimmer@gmail.com>
2021-01-01 16:18:14 -0600

    added total revenue

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,37 @@
-#import csv data to read
+# Import csv data to read
 import os
 import csv
 
+# Path to collect data from the Resources folder
 csvpath = os.path.join('..', '..','..', 'nu_bootcamp', 'nu-chi-data-pt-11-2020-u-c', 'Homework', '03-Python', 'Instructions', 'PyBank', 'Resources', 'budget_data.csv')
+
+# Open and read the CSV file
 with open(csvpath) as csvfile:
 
+    # Split the data by a comma
     csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Remove the header
     header = next(csvreader)
-    
+
+    # Assign values to variables
     total_months = 0
+    total_revenue = 0
     
+    # Loop through the data
     for row in csvreader:
         
+        # Count total months
         total_months = total_months + 1
 
-    #def totals(csvreader)
-       # Date= str(csvreader[0])
-        #Profits/Losses= int(csvreader[1])
+        # Add total revenue
+        total_revenue = total_revenue + int(row[1])
 
-    #total_months= len(list(csvreader))
+
 
     #--------------------------------
 
 print("Financial Analysis")
 print("-----------------------")
 print(f"Total Months: {total_months}")
+print(f"Total Revenue: ${total_revenue}")
